ab290d5b03e887d661fa7762a5d197cc5ab8225a
Edwards Presume <edwardspresume@gmail.com>
2023-03-21 14:21:04 -0400

    removed tailwind, added sass, added styling

--- src/components/ProjectProposalForm.svelte
+++ src/components/ProjectProposalForm.svelte
@@ -28,6 +28,10 @@
 </script>
 
 <form on:submit={submit}>
+    {#if responseMessage}
+        <p class="response_message">{responseMessage}</p>
+    {/if}
+
     <label>
         Title
         <input
@@ -36,7 +40,7 @@
             id="title"
             name="title"
             minlength="3"
-            placeholder="Enter the idea title"
+            placeholder="Enter the project title"
         />
     </label>
 
@@ -46,14 +50,74 @@
             required
             id="description"
             name="description"
+            rows="5"
             minlength="10"
-            placeholder="Enter the idea description"
+            placeholder="Enter the project description"
         />
     </label>
 
     <button type="submit">Submit Proposal</button>
-
-    {#if responseMessage}
-        <p>{responseMessage}</p>
-    {/if}
 </form>
+
+<style>
+    form {
+        padding: 40px;
+        margin: 4rem auto;
+        max-width: 550px;
+        border-radius: 10px;
+        background: rgba(0, 0, 0, 0.4);
+        box-shadow: 0 15px 25px rgba(0, 0, 0, 0.4);
+    }
+
+    .response_message {
+        font-weight: bold;
+        margin-bottom: 3rem;
+    }
+
+    label {
+        display: grid;
+        row-gap: 0.6rem;
+        font-size: 1.6rem;
+        margin-bottom: 3.5rem;
+    }
+
+    input,
+    textarea {
+        padding: 0.6rem;
+        border-radius: 6px;
+        border: 1px solid #ccc;
+    }
+
+    button {
+        width: 100%;
+        font-size: 2rem;
+        padding: 0.9rem;
+        text-align: center;
+        border-radius: 7px;
+        transition: all 0.8s;
+        text-transform: uppercase;
+        background-color: hsl(217, 91%, 35%);
+        background: linear-gradient(to right, #cb3066, #0e5cd1);
+        background-size: 400% 400%;
+        box-shadow: 0 3px 10px 0 rgba(205, 209, 215, 0.1);
+        animation: gradient 2.7s ease infinite;
+    }
+
+    button:hover {
+        transform: scale(1.01);
+        background: none;
+        background-color: #000;
+    }
+
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+</style>
